7c1769bdbd2776579886864d3d465f3b87aaa05d
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-01-23 11:00:59 +0200

    Change to c file

--- src/openvpn/comp.c
+++ src/openvpn/comp.c
@@ -1,4 +1,5 @@
 /*
+ * Comment
  *  OpenVPN -- An application to securely tunnel IP networks
  *             over a single UDP port, with support for SSL/TLS-based
  *             session authentication and key exchange,
