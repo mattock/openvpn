44a8f569de7a08819847e6f3fe74a3372ec62b5e
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-01-30 14:45:28 +0200

    Trigger build

--- src/openvpn/shaper.c
+++ src/openvpn/shaper.c
@@ -1,5 +1,6 @@
 /*
  * comment
+ * another one
  *  OpenVPN -- An application to securely tunnel IP networks
  *             over a single UDP port, with support for SSL/TLS-based
  *             session authentication and key exchange,
