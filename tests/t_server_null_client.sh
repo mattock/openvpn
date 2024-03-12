076790abb8e62be168396b94fedd6a9979ce58bf
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-03-12 15:23:13 +0200

    WIP: --dev null: support multiple client configs
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- tests/t_server_null_client.sh
+++ tests/t_server_null_client.sh
@@ -1,10 +1,38 @@
 #!/bin/sh
 
+do_client_test() {
+    TEST_NAME=$1
+
+    "${OPENVPN}" \
+        $CLIENT_BASE_OPTS \
+        $CLIENT_PROTO_OPTS \
+        $CLIENT_REMOTE_OPTS \
+        $CLIENT_CERT_OPTS \
+        $CLIENT_CONNECT_OPTS \
+        $CLIENT_LOG_OPTS \
+        $CLIENT_SCRIPT_OPTS
+
+    grep "Initialization Sequence Completed" "${LOG}" > /dev/null
+
+    if [ $? -eq 0 ]; then
+        echo "Success: ${TEST_NAME}"
+
+    else
+        echo "Fail: ${TEST_NAME}"
+        RETVAL=1
+    fi
+
+    rm -f "${LOG}"
+}
+
 . ./t_server_null.vars
 
 LOG="./test-client.log"
 
 rm -f "${LOG}"
+
+# Return value for the entire test suite. Gets set to 1 if any test fails.
+export RETVAL=0
 
 CLIENT_BASE_OPTS="--client --dev null --ifconfig-noexec --nobind --persist-tun --verb 3"
 CLIENT_PROTO_OPTS="--proto udp --cipher AES-256-CBC"
@@ -14,25 +42,13 @@ CLIENT_CONNECT_OPTS="--resolv-retry 0 --connect-retry-max 3 --server-poll-timeou
 CLIENT_LOG_OPTS="--log ${LOG}"
 CLIENT_SCRIPT_OPTS="--script-security 2 --up null_client_up.sh"
 
-"${OPENVPN}" \
-    $CLIENT_BASE_OPTS \
-    $CLIENT_PROTO_OPTS \
-    $CLIENT_REMOTE_OPTS \
-    $CLIENT_CERT_OPTS \
-    $CLIENT_CONNECT_OPTS \
-    $CLIENT_LOG_OPTS \
-    $CLIENT_SCRIPT_OPTS
+do_client_test base_1194
 
-grep "Initialization Sequence Completed" "${LOG}" > /dev/null
+CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1195 udp --remote-cert-tls server"
+do_client_test base_1195
 
-if [ $? -eq 0 ]; then
-    echo "Client connection successful"
-    RETVAL=0
-else
-    echo "Client connection could not be established"
-    RETVAL=1
-fi
 
-rm -f "${LOG}"
+CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1194 udp --remote-cert-tls server"
+do_client_test base_1194_2
 
 exit $RETVAL
