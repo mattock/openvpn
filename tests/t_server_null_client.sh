400f194530f10c387d5589280dba4e901f459ead
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-03-19 10:09:02 +0200

    WIP: disable broken --dev null tests
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- tests/t_server_null_client.sh
+++ tests/t_server_null_client.sh
@@ -36,19 +36,15 @@ export RETVAL=0
 
 CLIENT_BASE_OPTS="--client --dev null --ifconfig-noexec --nobind --persist-tun --verb 3"
 CLIENT_PROTO_OPTS="--proto udp --cipher AES-256-CBC"
-CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1194 udp --remote-cert-tls server"
 CLIENT_CERT_OPTS="--ca "${CA}" --cert "${CLIENT_CERT}" --key "${CLIENT_KEY}" --tls-auth "${TA}" 1"
 CLIENT_CONNECT_OPTS="--resolv-retry 0 --connect-retry-max 3 --server-poll-timeout 1 --explicit-exit-notify 3"
 CLIENT_LOG_OPTS="--log ${LOG}"
 CLIENT_SCRIPT_OPTS="--script-security 2 --up null_client_up.sh"
 
+CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1194 udp --remote-cert-tls server"
 do_client_test base_1194
 
-CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1195 udp --remote-cert-tls server"
-do_client_test base_1195
-
-
-CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1194 udp --remote-cert-tls server"
-do_client_test base_1194_2
+#CLIENT_REMOTE_OPTS="--remote 127.0.0.1 1195 udp --remote-cert-tls server"
+#do_client_test base_1195
 
 exit $RETVAL
