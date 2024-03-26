2c59150a23ca35d78173362594dd56551f4a149e
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-03-26 09:25:41 +0200

    Use test-specific log files in t_server_null_client.sh
    This paves the way for parallerizing the tests
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- tests/t_server_null_client.sh
+++ tests/t_server_null_client.sh
@@ -29,8 +29,6 @@ do_client_test() {
         cat "${log}"
         retval=1
     fi
-
-    rm -f "${log}"
 }
 
 srcdir="${srcdir:-.}"
@@ -41,9 +39,6 @@ ca="${ca:-${sample_keys}/ca.crt}"
 client_cert="${client_cert:-${sample_keys}/client.crt}"
 client_key="${client_key:-${sample_keys}/client.key}"
 ta="${ta:-${sample_keys}/ta.key}"
-log="${log:-${srcdir}/test-client.log}"
-
-rm -f "${log}"
 
 # Return value for the entire test suite. Gets set to 1 if any test fails.
 export retval=0
@@ -52,26 +47,31 @@ client_base_opts="--client --dev null --ifconfig-noexec --nobind --persist-tun -
 client_proto_opts="--proto udp --cipher AES-256-CBC"
 client_cert_opts="--ca "${ca}" --cert "${client_cert}" --key "${client_key}" --tls-auth "${ta}" 1"
 client_connect_opts="--resolv-retry 0 --connect-retry-max 3 --server-poll-timeout 1 --explicit-exit-notify 3"
-client_log_opts="--log ${log}"
 client_script_opts="--script-security 2 --up null_client_up.sh"
 
 # Cache the path current (compiled) openvpn
 current_openvpn=$openvpn
 
+test_name="t_server_null_client.sh-openvpn_current"
 openvpn=$current_openvpn
 client_remote_opts="--remote 127.0.0.1 1194 udp --remote-cert-tls server"
+client_log_opts="--log ${test_name}.log"
 should_succeed=0
-do_client_test t_server_null_client.sh-openvpn_current $should_succeed
+do_client_test "${test_name}" $should_succeed
 
 # Test for failure
+test_name="t_server_null_client.sh-openvpn_current_fail"
 openvpn=$current_openvpn
 client_remote_opts="--remote 127.0.0.1 1195 udp --remote-cert-tls server"
+client_log_opts="--log ${test_name}.log"
 should_succeed=1
-do_client_test t_server_null_client.sh-openvpn_current_fail $should_succeed
+do_client_test "${test_name}" $should_succeed
 
+test_name="t_server_null_client.sh-openvpn_2_6_8"
 openvpn="/usr/sbin/openvpn"
 client_remote_opts="--remote 127.0.0.1 1194 udp --remote-cert-tls server"
 should_succeed=0
-do_client_test t_server_null_client.sh-openvpn_2_6_8 $should_succeed
+client_log_opts="--log ${test_name}.log"
+do_client_test "${test_name}" $should_succeed
 
 exit $retval
