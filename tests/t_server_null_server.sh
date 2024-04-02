29e3bb2e35c54feb2c9ca9dc4417a55d7ecb4efe
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-04-02 12:39:20 +0300

    Make --dev null servers exit faster
    The previous refactored version had the drawback that we'd wait for each server
    to stop before moving on to the next one. This would have been very slow as
    server count increased. Now poll for clients in all status files, so that we can
    exit fast when no clients are connected to any server.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- tests/t_server_null_server.sh
+++ tests/t_server_null_server.sh
@@ -27,31 +27,43 @@ do
     launch_server "${server_name}" "${server_conf}"
 done
 
-# Wait for clients to disconnect
+# Create a list of status and pid files. The former allows checking "global" status of client
+# connections across --dev null test servers.
+status_files=""
+pid_files=""
 for SUF in $TEST_SERVER_LIST
 do
     eval server_name=\"\$SERVER_NAME_$SUF\"
 
-    status="${srcdir}/${server_name}.status"
-    pid="${srcdir}/${server_name}.pid"
+    status_files="${status_files} ${srcdir}/${server_name}.status"
+    pid_files="${pid_files} ${srcdir}/${server_name}.pid"
+done
 
-    # Wait until no clients are connected anymore, then exit
-    count=0
-    while [ $count -lt 10 ]; do
-        if grep -q "${CLIENT_MATCH}" "${status}"; then
-            count=0
-            sleep 1
-            continue
-        else
-            ((count++))
-            sleep 1
-            continue
-        fi
-    done
+# Wait until no clients are connected to any test server. Wait at least five seconds
+# to avoid killing the servers prematurely.
+count=0
+while [ $count -lt 5 ]; do
+    if cat $status_files|grep -q "${CLIENT_MATCH}"; then
+        count=0
+        sleep 1
+        continue
+    else
+        ((count++))
+        sleep 1
+        continue
+    fi
+done
 
-    kill `cat $pid`
+# All clients have now exited. Kill all server processes and remove their pid
+# files, if any.
+for PID_FILE in $pid_files
+do
+    kill `cat $PID_FILE`
+    test -r "${PID_FILE}" && rm -f "${PID_FILE}"
+done
 
-    test -f $pid && rm -f $pid
-    test -f $status && rm -f $status
-
+# Remove server status files, if present.
+for STATUS_FILE in $status_files
+do
+    test -r "${STATUS_FILE}" && rm -f "${STATUS_FILE}"
 done
