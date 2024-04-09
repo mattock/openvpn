4fe9e3aaf5f71b9b6b791b36f5e85372d5099d06
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-04-09 16:05:44 +0300

    Improvements to --dev null
    - Support management-based killing of servers
    - Add inactivity timeout
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- tests/t_server_null_server.sh
+++ tests/t_server_null_server.sh
@@ -33,41 +33,50 @@ done
 
 # Create a list of status and pid files. The former allows checking "global" status of client
 # connections across --dev null test servers.
-status_files=""
-pid_files=""
+export status_files=""
+export pid_files=""
+export mgmt_ports=""
 for SUF in $TEST_SERVER_LIST
 do
     eval server_name=\"\$SERVER_NAME_$SUF\"
+    eval mgmt_port=\"\$SERVER_MGMT_PORT_$SUF\"
 
     status_files="${status_files} ${srcdir}/${server_name}.status"
     pid_files="${pid_files} ${srcdir}/${server_name}.pid"
+    mgmt_ports="${mgmt_ports} ${mgmt_port}" 
 done
 
 # Wait until no clients are connected to any test server. Wait at least five seconds
 # to avoid killing the servers prematurely.
 count=0
-while [ $count -lt 10 ]; do
-    if cat $status_files|grep -q "${CLIENT_MATCH}"; then
+while [ $count -lt 5 ]; do
+    if cat $status_files|grep -qE "(${CLIENT_MATCH}|UNDEF)"; then
+	echo "Clients connected, count reset to 0"
         count=0
         sleep 1
         continue
     else
+	echo "No clients connected, count at ${count}"
+	#cat $status_files
         ((count++))
         sleep 1
         continue
     fi
 done
 
-# All clients have now exited. Kill all server processes and remove their pid
-# files, if any.
+echo "All clients disconnected from all servers"
+
+#sleep 300
+
+for MGMT_PORT in $mgmt_ports
+do
+    echo "signal SIGTERM"|nc 127.0.0.1 $MGMT_PORT
+done
+
+# Pidfile-based approach seems unreliable. If it fails once, the game seems to
+# be over.  However, it is still useful as a fallback in case management
+# interface is not available.
 for PID_FILE in $pid_files
 do
     kill `cat $PID_FILE`
-    test -r "${PID_FILE}" && rm -f "${PID_FILE}"
-done
-
-# Remove server status files, if present.
-for STATUS_FILE in $status_files
-do
-    test -r "${STATUS_FILE}" && rm -f "${STATUS_FILE}"
 done
