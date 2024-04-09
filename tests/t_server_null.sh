cd0ca91f090b5d84c13b81dd665ce295e97d98f2
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-04-09 12:12:56 +0300

    Support running --dev null tests as root (without sudo)
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- tests/t_server_null.sh
+++ tests/t_server_null.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 if [ `id -un` == "root" ]; then
-    sudo_cmd=""
+    use_sudo="no"
 else
+    use_sudo="yes"
     sudo_cmd=`which sudo`
     if [ $? -ne 0 ]; then
         echo "ERROR: $0: not running as root and sudo not found!"
@@ -13,7 +14,11 @@ fi
 
 srcdir="${srcdir:-.}"
 
-$sudo_cmd "${srcdir}/t_server_null_server.sh"
+if [ "${use_sudo}" = "yes" ]; then
+    $sudo_cmd "${srcdir}/t_server_null_server.sh"
+else
+    "${srcdir}/t_server_null_server.sh" &
+fi
 
 "${srcdir}/t_server_null_client.sh"
 
